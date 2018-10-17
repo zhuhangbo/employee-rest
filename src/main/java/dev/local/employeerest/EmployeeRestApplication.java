88a47d84b37370a908146641edd7a2905b2494f0
zhuhangbo <zhuhangbo2917@163.com>
2018-10-17 16:15:10 +0800

    init project

---
+++ src/main/java/dev/local/employeerest/EmployeeRestApplication.java
@@ -0,0 +1,11 @@
+package dev.local.employeerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestApplication.class, args);
+	}
+}
